043153325fd87b407587253b2fd834c30d7fa1f9
Bayna0999 <baynaa123455@gmail.com>
2025-09-24 22:48:23 +0800

    mailer commented

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,30 +1,30 @@
-import { Usermodel } from "../model/user.js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import nodemailer from "nodemailer";
-import { sendMail } from "../utils/sendmailer.js";
+import { Usermodel } from '../model/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+// import nodemailer from 'nodemailer';
+// import { sendMail } from '../utils/sendmailer.js';
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
   const secret_key = process.env.SECRET_KEY;
   try {
-    const user = await Usermodel.findOne({ email: email }).select("+password");
+    const user = await Usermodel.findOne({ email: email }).select('+password');
     console.log(user);
 
     if (!user) {
       return res.status(400).send({
         success: false,
-        message: "User not found",
+        message: 'User not found',
       });
     }
-    console.log(password, user.password, "password");
+    console.log(password, user.password, 'password');
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log(isMatch, "FROM BCRYPT");
+    console.log(isMatch, 'FROM BCRYPT');
 
     if (!isMatch) {
       return res.status(401).send({
         success: false,
-        message: "Email or password is incorrect",
+        message: 'Email or password is incorrect',
       });
     }
     const token = jwt.sign({ ...user }, secret_key, { expiresIn: 3600 });
@@ -33,10 +33,10 @@ export const login = async (req, res) => {
       token,
     });
   } catch (error) {
-    console.error("login error:", error);
+    console.error('login error:', error);
     return res.status(500).send({
       success: false,
-      message: "Email or password is incorrect",
+      message: 'Email or password is incorrect',
     });
   }
 };
@@ -52,15 +52,15 @@ export const login = async (req, res) => {
 //   },
 // });
 
-export const sendMailer = async (req, res) => {
-  const { email, text, subject } = req.body;
-  try {
-    const response = await sendMail(email, subject, text);
-    res.status(200).send({ success: true, data: response });
-  } catch (error) {
-    res.status(400).send({ success: false, error: error });
-  }
-};
+// export const sendMailer = async (req, res) => {
+//   const { email, text, subject } = req.body;
+//   try {
+//     const response = await sendMail(email, subject, text);
+//     res.status(200).send({ success: true, data: response });
+//   } catch (error) {
+//     res.status(400).send({ success: false, error: error });
+//   }
+// };
 // const info = {
 //   from: '"Maddison Foo Koch 👻" <baynaa123455@gmail.com>', // sender address
 //   to: "baynaa9998837@gmail.com, bayarjavkhlan8005@gmail.com", // list of receivers
